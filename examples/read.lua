b48434f5f83760fd53b7184b91eccdf2ef940640
zeykatecool <68609058+zeykatecool@users.noreply.github.com>
2024-08-09 17:43:06 +0300

    Update read.lua

--- examples/read.lua
+++ examples/read.lua
@@ -2,6 +2,4 @@ local Logger = require("logger")
 local inspect = require("inspect") --https://github.com/kikito/inspect.lua
 
 local luaTable = Logger:read("myLog.log")
-print(inspect(luaTable)) -- You can convert log files to log_Object like this.
-
-
+print(inspect(luaTable)) -- You can read log files (log_Object.logs) in lua table like this.
