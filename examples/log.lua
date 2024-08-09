dda21dcecc46bed0b1f38a56bba40bf72eb75957
zeykatecool <68609058+zeykatecool@users.noreply.github.com>
2024-08-09 17:43:53 +0300

    Update log.lua

--- examples/log.lua
+++ examples/log.lua
@@ -1,5 +1,5 @@
 local Logger = require("logger")
 
-local myLog = Logger:newLogger("myLog.log")
+local myLog = Logger:newLogger("myLog.log") -- It will load data from myLog.log if file exist.If file does not exist,it will create a new one.
 Logger:log(myLog, "This is Message Of Log", "This is a Description Of Log", Logger.levels.DEBUG)
 myLog:log("This is Message Of Log", "This is a Description Of Log", Logger.levels.DEBUG)
